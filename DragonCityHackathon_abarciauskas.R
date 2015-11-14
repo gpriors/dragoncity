cd8d170e0d11cf92ccf47d0d02c72154bc9c7e4d
Aimee Barciauskas <abarciauskas@mdsol.com>
2015-11-14 11:22:53 +0100

    Modified baseline code

---
+++ DragonCityHackathon_abarciauskas.R
@@ -0,0 +1,49 @@
+##################################################################
+# WELCOME TO THE BCNANALYTICS AND SOCIAL POINT DATA HACKATHON
+# In this file you will find the code for the baseline model
+################################################################
+
+####################################################################
+#Reading in the data
+setwd('train_test')
+dragoncity_hackathon_train <- read.csv(file="dragon_city_hackathon_train_final.csv", header=TRUE,sep=",")
+dragoncity_hackathon_test <- read.csv(file="dragon_city_hackathon_test_final.csv", header=TRUE,sep=",")
+
+
+####################################################
+## fit model
+modelfit.baseline <- glm(churn ~ num_sessions+played_day2
+                +breedings+has_login_error
+                +reach_lvl_3+lvl_ups+attacks+device_group
+                +device_age,data=dragoncity_hackathon_train,family=binomial())
+summary(modelfit.baseline)
+
+#modelfit.all <- glm(churn ~ .,data=dragoncity_hackathon_train,family=binomial())
+#summary(modelfit.all)
+
+
+
+
+
+
+####################################################
+##Creating the predictions of the model
+predictions.baseline <- predict(modelfit, newdata = dragoncity_hackathon_test, type = "response")
+write.table(predictions.baseline, 'baseline_submission.csv', row.names=F, quote=F, col.names=F) 
+
+####################################################
+# #Checking the roc and auc
+# We suggest to check the AUC  in a crossvalidated set internally as
+# an alternativeway to check the quality of your model instead
+# of just relying on the public leaderboard feedback
+
+# You can use the auc function of the pROC package for it
+#install.packages('pROC')
+install.packages("pROC")
+library(pROC)
+auc(pred, real)
+
+install.packages("ROCR")
+library(ROCR)
+ROCRpred = prediction(predictions, train$churn)
+as.numeric(performance(ROCRpred, "auc")@y.values)
